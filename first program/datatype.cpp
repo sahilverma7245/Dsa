3e430f2eac565347862fcd9cbc0ff83aeeafefb9
priyanshu <priyanshuverma7245@gmail.com>
2023-12-17 11:42:33 +0530

    mess

---
+++ first program/datatype.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a = 12;
+   cout<<a<<endl;
+
+   char b = 65;
+   cout<<b<<endl;
+
+   float ca = 1.22;
+   cout<<ca<<endl;
+
+   double d = 2111;
+   cout<<d<<endl;
+
+
+  
+
+   return 0;
+}
